4034565d0ba0852f7262a606a73679cda5190cd8
alexandersuclupe <alexandersuclupe17@gmail.com>
2025-06-09 22:15:35 -0500

    ver_10

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventas.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
